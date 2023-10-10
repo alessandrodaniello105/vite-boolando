f6686fc9f2fe9a18caaab54f7c3e90d9f2ee1c53
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-10 14:38:48 +0200

    [add]: static header markup in Header template

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,39 @@ export default {
 </script>
 
 <template>
-  <header>Header</header>
+  <header>
+     
+    <!-- top-bar -->
+    <div class="top-bar container d-flex">
+
+      <!-- WEBSITE MENU -->
+      <div class="menu website">
+        <ul>
+          <li><a href="#">Donna</a></li>
+          <li><a href="#">Uomo</a></li>
+          <li><a href="#">Bambini</a></li>
+        </ul>
+      </div>
+      <!-- /WEBSITE MENU -->
+
+      <!-- LOGO -->
+      <div class="logo">
+        <a href="index-alt.html"><img src="assets/img/boolean-logo.png" alt="Logo Boolean"></a>
+      </div>
+      <!-- /LOGO -->
+      
+      <!-- USER MENU -->
+      <div class="menu user">
+        <a href="#"><i class="fa-regular fa-user"></i></a>
+        <a href="#"><i class="fa-regular fa-heart"></i></a>
+        <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+      </div>
+      <!-- /USER MENU -->
+      
+    </div>
+    <!-- top-bar -->
+
+  </header>
 </template>
 
 <style>
