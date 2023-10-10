de93c2fcdd9ae792e3b3d9d4522c46d0c94b0508
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-10 15:47:37 +0200

    [add]: productInfo, isDiscounted and price

--- src/data/productCards.js
+++ src/data/productCards.js
@@ -4,58 +4,84 @@ export default [
     urlA: '../src/assets/img/1.webp',
     urlB: '../src/assets/img/1b.webp',
     productInfo: {
-      brand: '',
-      model: ''
+      brand: 'Levi\'s',
+      model: 'Relaxed fit tee unisex'
     },
     isFav: false,
-    discountPercentage: 0,
-    isEco: false,
-    price: 0
+    isDiscounted: true,
+    discountPercentage: 50,
+    isEco: true,
+    price: 29.99
   },
 
   {
     urlA: '../src/assets/img/2.webp',
     urlB: '../src/assets/img/2b.webp',
+    productInfo: {
+      brand: 'Guess',
+      model: 'Roses Tee'
+    },
     isFav: false,
-    discountPercentage: 0,
+    isDiscounted: true,
+    discountPercentage: 30,
     isEco: false,
-    price: 0
+    price: 29.99
   },
 
   {
     urlA: '../src/assets/img/3.webp',
     urlB: '../src/assets/img/3b.webp',
+    productInfo: {
+      brand: 'Come zucchero Filato',
+      model: 'Voglia di colori pastello'
+    },
     isFav: false,
-    discountPercentage: 0,
+    isDiscounted: true,
+    discountPercentage: 30,
     isEco: false,
-    price: 0
+    price: 104.99
   },
 
   {
     urlA: '../src/assets/img/4.webp',
     urlB: '../src/assets/img/4b.webp',
+    productInfo: {
+      brand: 'Levi\'s',
+      model: 'tee unisex'
+    },
     isFav: false,
-    discountPercentage: 0,
-    isEco: false,
-    price: 0
+    isDiscounted: true,
+    discountPercentage: 50,
+    isEco: true,
+    price: 29.99
   },
 
   {
     urlA: '../src/assets/img/5.webp',
     urlB: '../src/assets/img/5b.webp',
+    productInfo: {
+      brand: 'Maya Deluxe',
+      model: 'Strip bodice'
+    },
     isFav: false,
+    isDiscounted: false,
     discountPercentage: 0,
     isEco: false,
-    price: 0
+    price: 99.99
   },
 
   {
     urlA: '../src/assets/img/6.webp',
     urlB: '../src/assets/img/6b.webp',
+    productInfo: {
+      brand: 'Esprit',
+      model: 'Maglione - black'
+    },
     isFav: false,
+    isDiscounted: false,
     discountPercentage: 0,
-    isEco: false,
-    price: 0
+    isEco: true,
+    price: 29.99
   },
 
 ]
