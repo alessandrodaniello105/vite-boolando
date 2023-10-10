9907f7aeb73e0779575350407d1efd63815b5420
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-10 15:30:15 +0200

    [add]: products infos in \data\productCards.js

---
+++ src/data/productCards.js
@@ -0,0 +1,61 @@
+export default [
+
+  {
+    urlA: '../src/assets/img/1.webp',
+    urlB: '../src/assets/img/1b.webp',
+    productInfo: {
+      brand: '',
+      model: ''
+    },
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+  {
+    urlA: '../src/assets/img/2.webp',
+    urlB: '../src/assets/img/2b.webp',
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+  {
+    urlA: '../src/assets/img/3.webp',
+    urlB: '../src/assets/img/3b.webp',
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+  {
+    urlA: '../src/assets/img/4.webp',
+    urlB: '../src/assets/img/4b.webp',
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+  {
+    urlA: '../src/assets/img/5.webp',
+    urlB: '../src/assets/img/5b.webp',
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+  {
+    urlA: '../src/assets/img/6.webp',
+    urlB: '../src/assets/img/6b.webp',
+    isFav: false,
+    discountPercentage: 0,
+    isEco: false,
+    price: 0
+  },
+
+]
